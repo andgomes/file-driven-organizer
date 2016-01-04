bb79cb46ad3792d31f6b256769b6a92e090dd406
Anderson Gomes <gomes.andrade1@gmail.com>
2016-01-04 20:19:33 -0300

    Configuração de build inicial mas ainda não funcionando

---
+++ src/main/com/andgomes/core/HelloApplication.java
@@ -0,0 +1,11 @@
+package com.andgomes.core;
+
+public class HelloApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Application!");
+
+	}
+
+}
---
+++ src/test/com/andgomes/core/HelloApplicationTest.java
@@ -0,0 +1,14 @@
+package com.andgomes.core;
+
+import org.junit.Test;
+
+public class HelloApplicationTest {
+
+	@Test
+	public void test() {
+
+		assertTrue(true);
+
+	}
+
+}
